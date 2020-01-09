1939f832468d4404032746cfa7d5e7c1e6c009d9
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2020-01-09 11:09:38 -0500

    iu

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("./db/mongoose");  //ensures mongoose runs and connects to database
 const app = express();
+const movieRouter = require("./routers/movies");
+app.use(express.json());
+app.use(movieRouter);
 app.listen(3000, () => {
     console.log("Server up on 3000");
 });
+
 
 
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.connect("paste something here", 
-{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true
-});
+mongoose.connect("mongodb+srv://dejon:Smile1@cluster0-kowtm.mongodb.net/test?retryWrites=true&w=majority", 
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    }
+);
+
+
+
+
+
+
