452a00103d28aff1b8691a0b8b1db2f46f5c1383
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2020-01-16 11:44:24 -0500

    W

--- src/index.js
+++ src/index.js
@@ -9,3 +9,21 @@ app.use(userRouter);
 app.listen(3000, () => {
     console.log("Server up on 3000");
 });
+
+
+/* const jwt = require("jsonwebtoken");
+
+const testFunction = async () => {
+    const token = jwt.sign({ _id:"5e208c64e39a8e2e404d1d5c"}, "obeysudo", {
+        expiresIn: "7 days"
+    });
+    console.log(token);
+    const data = jwt.verify(token, "obeysudo");
+    console.log(data);
+};
+
+testFunction(); */
+
+
+
+
--- src/routers/user.js
+++ src/routers/user.js
@@ -60,7 +60,8 @@ router.post("/users", async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
-    res.send(user);
+    const token = await user.generateToken(); //lowercase "token" so that token is generated for only this user
+    res.send({user, token});
   } catch (e) {
     res.status(400).send(e);
   }
@@ -87,6 +88,11 @@ router.get("/users", async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+router.get("/users/me", auth, async(req, res) => {
+  res.send(req.user);
+});
+
 router.get("/users/:id", async (req, res) => {
   try {
     let user = await User.findyyId(req.params.id);
